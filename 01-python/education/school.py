c18d346e534839c125c3cde1cbec5e472f26248a
ChuJiaWen <sab1025@bu.edu>
2021-09-23 17:24:44 -0400

    Added print statements to school.py

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,3 +1,8 @@
 def draw_school():
-    print("school not found")
+    print("Welcome to Boston Primary School")
+    print("Here is our teaching building")
+    print("This is a Science building!")
+    print("This is the dormitory girls!")
+    print("This is the dorm for boys!")
+    print("Thank you for your time~ Bye Bye~")
     return
