630dc0b60563f6b63c8fe60ce08d6a5ff7b4483b
ChuJiaWen <sab1025@bu.edu>
2021-09-30 20:46:45 -0400

    Updated sim.py to calculate Euclidean& Manhattan& Jaccard Distance, and Cosine Similarity

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,12 +5,21 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    intersection = len(list(set(x).intersection(y)))
+    union = (len(x) + len(y)) - intersection
+    return 1-(float(intersection)/union)
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    dot = sum(a*b for a, b in zip(x, y))
+    norm_x = sum(a*a for a in x) ** 0.5
+    norm_y = sum(b*b for b in y) ** 0.5
+
+    return dot / (norm_x*norm_y)
 
 # Feel free to add more
